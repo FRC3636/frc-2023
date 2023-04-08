8a7513a321479678ae011ef570b616d52d8fc2d3
gagnonsilas <gagnon.silas@gmail.com>
2023-04-08 09:22:01 -0700

    fix: remove game piece detection from auto score

--- src/main/java/frc/robot/commands/autonomous/AutoScore.java
+++ src/main/java/frc/robot/commands/autonomous/AutoScore.java
@@ -15,26 +15,24 @@ import java.util.function.Supplier;
 //Move to a node, position the arm and outtake the current game piece
 public class AutoScore extends SequentialCommandGroup {
     public AutoScore(Drivetrain drivetrain, Arm arm, PoseEstimation poseEstimation, Supplier<Node> targetNode){
-        if (arm.getRollers().isHoldingGamePiece()) {
-            this.addCommands(
-                    new AlignToSelectedNode(drivetrain, arm, poseEstimation, targetNode, 0.75)
-                            .raceWith(
-                                    new SequentialCommandGroup(
-                                            new WaitUntilCommand(() -> drivetrain.getRelativeVelocity().getNorm() > 0.05),
-                                            new WaitUntilCommand(() -> drivetrain.getRelativeVelocity().getNorm() < 0.05),
-                                            new InstantCommand(() -> arm.setRollerState(Rollers.State.Outtake)),
-                                            new WaitCommand(0.25),
-                                            new InstantCommand(() -> arm.setRollerState(Rollers.State.Off))
-                                    )
-                            ),
-                    new InstantCommand(() ->
-                            new WaitUntilCommand(
-                                    () -> AllianceUtils.getDistanceFromAlliance(poseEstimation.getEstimatedPose()) > Constants.Arm.SAFE_RAISING_DISTANCE
-                            ).andThen(
-                                    new InstantCommand(() -> arm.setTarget(Arm.State.Stowed))
-                            ).schedule()
-                    )
-            );
-        }
+        this.addCommands(
+                new AlignToSelectedNode(drivetrain, arm, poseEstimation, targetNode, 0.75)
+                        .raceWith(
+                                new SequentialCommandGroup(
+                                        new WaitUntilCommand(() -> drivetrain.getRelativeVelocity().getNorm() > 0.05),
+                                        new WaitUntilCommand(() -> drivetrain.getRelativeVelocity().getNorm() < 0.05),
+                                        new InstantCommand(() -> arm.setRollerState(Rollers.State.Outtake)),
+                                        new WaitCommand(0.25),
+                                        new InstantCommand(() -> arm.setRollerState(Rollers.State.Off))
+                                )
+                        ),
+                new InstantCommand(() ->
+                        new WaitUntilCommand(
+                                () -> AllianceUtils.getDistanceFromAlliance(poseEstimation.getEstimatedPose()) > Constants.Arm.SAFE_RAISING_DISTANCE
+                        ).andThen(
+                                new InstantCommand(() -> arm.setTarget(Arm.State.Stowed))
+                        ).schedule()
+                )
+        );
     }
 }
